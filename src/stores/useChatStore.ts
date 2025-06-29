5ff3c5b0973510622356d12559aeba914535fffa
ashishkumarchandan <aaassshhhiiissshhh111@gmail.com>
2025-06-29 19:11:22 +0530

    sotreDone

--- src/stores/useChatStore.ts
+++ src/stores/useChatStore.ts
@@ -1,18 +1,25 @@
 import api from "@/lib/axios";
 import { create } from "zustand";
 import { io } from "socket.io-client";
-import { Message, User } from "@/types";
+import type { Message, User } from "@/types";
 
 interface Chatstore {
-  user: User[];
-  isLoading: boolean;
-  error: string | null;
-  socket: any;
-  isConnected: boolean;
-  onlineUsers: Set<string>;
-  userActivities: Map<string, string>;
-  messages: Message[];
-  selectedUser: User | null;
+  users: User[];
+	isLoading: boolean;
+	error: string | null;
+	socket: any;
+	isConnected: boolean;
+	onlineUsers: Set<string>;
+	userActivities: Map<string, string>;
+	messages: Message[];
+	selectedUser: User | null;
+
+	fetchUsers: () => Promise<void>;
+	initSocket: (userId: string) => void;
+	disconnectSocket: () => void;
+	sendMessage: (receiverId: string, senderId: string, content: string) => void;
+	fetchMessages: (userId: string) => Promise<void>;
+	setSelectedUser: (user: User | null) => void;
 }
 
 const baseURL = import.meta.env.VITE_BACKEND_BASE_URL;
@@ -22,4 +29,126 @@ const socket = io(baseURL, {
   withCredentials: true,
 });
 
-export const useChatStore = create<Chatstore>((set, get) => ({}));
+export const useChatStore = create<Chatstore>((set, get) => ({
+  users: [],
+	isLoading: false,
+	error: null,
+	socket: socket,
+	isConnected: false,
+	onlineUsers: new Set(),
+	userActivities: new Map(),
+	messages: [],
+	selectedUser: null,
+
+  setSelectedUser: (user) => set({ selectedUser: user }),
+
+  fetchUsers: async () => {
+    set({
+      isLoading: true,
+      error: null,
+    });
+
+    try {
+      const response = await api.get("/users");
+      set({
+        users: response.data,
+      });
+    } catch (error: any) {
+      set({
+        error: error.response.data.message,
+      });
+    } finally {
+      set({
+        isLoading: false,
+      });
+    }
+  },
+
+  initSocket: (userId) => {
+    if (!get().isConnected) {
+      socket.auth = { userId };
+      socket.connect();
+
+      socket.emit("user_connected", userId);
+
+      socket.on("users_online", (users: string[]) => {
+        set(() => ({
+          onlineUsers: new Set(users),
+        }));
+      });
+
+      socket.on("activities", (activities: [string, string][]) => {
+        set({
+          userActivities: new Map(activities),
+        });
+      });
+
+      socket.on("user_connected", (userId: string) => {
+        set((state) => {
+          const newOnlineUsers = new Set(state.onlineUsers);
+          newOnlineUsers.add(userId);
+
+          return {
+            onlineUsers: newOnlineUsers,
+          };
+        });
+      });
+
+      socket.on("user_disconnected", (userId: string) => {
+        set((state) => {
+          const newOnlineUsers = new Set(state.onlineUsers);
+          newOnlineUsers.delete(userId);
+          return { onlineUsers: newOnlineUsers };
+        });
+      });
+
+      socket.on("receive_message", (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }));
+      });
+
+      socket.on("message_sent", (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }));
+      });
+
+      socket.on("activity_updated", ({ userId, activity }) => {
+        set((state) => {
+          const newActivities = new Map(state.userActivities);
+          newActivities.set(userId, activity);
+          return { userActivities: newActivities };
+        });
+      });
+
+      set({ isConnected: true });
+    }
+  },
+
+  disconnectSocket: () => {
+    if (get().isConnected) {
+      socket.disconnect();
+      set({ isConnected: false });
+    }
+  },
+
+  sendMessage: async (receiverId, senderId, content) => {
+    const socket = get().socket;
+    if (!socket) return;
+
+    socket.emit("send_message", { receiverId, senderId, content });
+  },
+
+  fetchMessages: async (userId: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await api.get(`/users/messages/${userId}`);
+      set({ messages: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
--- src/stores/usePlayerStore.ts
+++ src/stores/usePlayerStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { Song } from "@/types";
+import type { Song } from "@/types";
 import { useChatStore } from "./useChatStore";
 
 interface PlayerStore {
@@ -34,14 +34,15 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		if (songs.length === 0) return;
 
 		const song = songs[startIndex];
-
 		const socket = useChatStore.getState().socket;
+
 		if (socket.auth) {
 			socket.emit("update_activity", {
 				userId: socket.auth.userId,
 				activity: `Playing ${song.title} by ${song.artist}`,
 			});
 		}
+
 		set({
 			queue: songs,
 			currentSong: song,
@@ -62,6 +63,7 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		}
 
 		const songIndex = get().queue.findIndex((s) => s._id === song._id);
+
 		set({
 			currentSong: song,
 			isPlaying: true,
@@ -71,31 +73,30 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 
 	togglePlay: () => {
 		const willStartPlaying = !get().isPlaying;
-
 		const currentSong = get().currentSong;
 		const socket = useChatStore.getState().socket;
+
 		if (socket.auth) {
 			socket.emit("update_activity", {
 				userId: socket.auth.userId,
 				activity:
-					willStartPlaying && currentSong ? `Playing ${currentSong.title} by ${currentSong.artist}` : "Idle",
+					willStartPlaying && currentSong
+						? `Playing ${currentSong.title} by ${currentSong.artist}`
+						: "Idle",
 			});
 		}
 
-		set({
-			isPlaying: willStartPlaying,
-		});
+		set({ isPlaying: willStartPlaying });
 	},
 
 	playNext: () => {
 		const { currentIndex, queue } = get();
 		const nextIndex = currentIndex + 1;
+		const socket = useChatStore.getState().socket;
 
-		// if there is a next song to play, let's play it
 		if (nextIndex < queue.length) {
 			const nextSong = queue[nextIndex];
 
-			const socket = useChatStore.getState().socket;
 			if (socket.auth) {
 				socket.emit("update_activity", {
 					userId: socket.auth.userId,
@@ -109,27 +110,25 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 				isPlaying: true,
 			});
 		} else {
-			// no next song
-			set({ isPlaying: false });
-
-			const socket = useChatStore.getState().socket;
 			if (socket.auth) {
 				socket.emit("update_activity", {
 					userId: socket.auth.userId,
-					activity: `Idle`,
+					activity: "Idle",
 				});
 			}
+
+			set({ isPlaying: false });
 		}
 	},
+
 	playPrevious: () => {
 		const { currentIndex, queue } = get();
 		const prevIndex = currentIndex - 1;
+		const socket = useChatStore.getState().socket;
 
-		// theres a prev song
 		if (prevIndex >= 0) {
 			const prevSong = queue[prevIndex];
 
-			const socket = useChatStore.getState().socket;
 			if (socket.auth) {
 				socket.emit("update_activity", {
 					userId: socket.auth.userId,
@@ -143,16 +142,14 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 				isPlaying: true,
 			});
 		} else {
-			// no prev song
-			set({ isPlaying: false });
-
-			const socket = useChatStore.getState().socket;
 			if (socket.auth) {
 				socket.emit("update_activity", {
 					userId: socket.auth.userId,
-					activity: `Idle`,
+					activity: "Idle",
 				});
 			}
+
+			set({ isPlaying: false });
 		}
-	},
+	}
 }));
