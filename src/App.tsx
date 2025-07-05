2530165865e505cd8f1a4462059cda21e834a521
aaassshhhiiissshhh111 <aaassshhhiiissshhh111@gmail.com>
2025-07-05 17:11:47 +0000

    maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
-  const [count, setCount] = useState(0);
+	console.log(import.meta.env.VITE_BACKEND_BASE_URL);
+	return (
+		<>
+			<Routes>
+				<Route
+					path='/sso-callback'
+					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+				/>
+				<Route path='/auth-callback' element={<AuthCallbackPage />} />
+				<Route path='/admin' element={<AdminPage />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-amber-500">yo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+				<Route element={<MainLayout />}>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
--- src/layout/components/LeftSidebar.tsx
+++ src/layout/components/LeftSidebar.tsx
@@ -13,6 +13,7 @@ const LeftSidebar = () => {
 
 	useEffect(() => {
 		fetchAlbums();
+		console.log(albums);
 	}, [fetchAlbums]);
 
 	console.log({ albums });
